866e21df992f6c98b34ade246c8d3761bd153cf6
Delio-Fullstack <tablangdelio@gmail.com>
2022-02-18 17:50:07 +0800

    done with counter app

--- pages/counterapp.js
+++ pages/counterapp.js
@@ -7,25 +7,25 @@ const Counterapp = () => {
     const[ parity, setParity ] = useState('')
     
     useEffect(() => {
-        if(count % 2 == 0 ) {
-            setParity('even')
-        }else{
-            setParity('odd')
-        }
-     },[count]);
+
+        if(count % 2 == 0 ) return setParity('even')
+
+        return setParity('odd')
+
+     }, [ count ] );
 
     const handleAddCount = () => {
-        setCount(prev => prev + 1);
+
+        setCount (prev => prev + 1);
     }
     const handleSubtractCount = () => {
-        // when count is 0 to avoide negative integer return prev state
+        // when count is 0 to persist negative integer return prev state
         if(count === 0 ) return setCount(prev => prev )
-        
+
         setCount(prev => prev - 1)
+
     }
-    const handleReset = () => {
-        setCount(0)
-    }
+  
     
     return(
         <main className='main'>
@@ -34,7 +34,7 @@ const Counterapp = () => {
                 <span>{ count }</span> 
                 <Button label=" + " handleClick={handleAddCount} />
                 <Button label=" - " handleClick={handleSubtractCount} />
-                <Button label="reset" handleClick={handleReset} />
+                <Button label="reset" handleClick={() => {  setCount(0) } } />
             </div>
         </main>
     )
